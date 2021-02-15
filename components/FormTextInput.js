ca7906de8a600a00caa72c9c6eab593199d1fae8
Arsalan <arsalan_cr7@yahoo.com>
2021-02-15 21:24:37 +0500

    added post modify and delete

---
+++ components/FormTextInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Input} from 'react-native-elements';
+import PropTypes from 'prop-types';
+
+const FormTextInput = ({style, ...otherProps}) => {
+  return <Input {...otherProps} />;
+};
+
+FormTextInput.propTypes = {
+  style: PropTypes.object,
+};
+
+export default FormTextInput;
---
+++ components/LoginForm.js
@@ -0,0 +1,53 @@
+import React, {useContext, useState} from 'react';
+import {View, Alert} from 'react-native';
+import {Button} from 'react-native-elements';
+import PropTypes from 'prop-types';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useLogin} from '../hooks/ApiHooks';
+import FormTextInput from './FormTextInput';
+import useLoginForm from '../hooks/LoginHooks';
+
+const LoginForm = ({navigation}) => {
+  const [loading, setLoading] = useState(false);
+  const {inputs, handleInputChange} = useLoginForm();
+  const {postLogin} = useLogin();
+  const {setIsLoggedIn} = useContext(MainContext);
+
+  const doLogin = async () => {
+    setLoading(true);
+    try {
+      const userData = await postLogin(inputs);
+      setIsLoggedIn(true);
+      await AsyncStorage.setItem('userToken', userData.token);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error('postLogin error', error.message);
+      Alert.alert('Invalid username or password');
+    }
+  };
+
+  return (
+    <View>
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="username"
+        onChangeText={(txt) => handleInputChange('username', txt)}
+      />
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="password"
+        onChangeText={(txt) => handleInputChange('password', txt)}
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={doLogin} loading={loading} />
+    </View>
+  );
+};
+
+LoginForm.propTypes = {
+  navigation: PropTypes.object,
+};
+
+export default LoginForm;
